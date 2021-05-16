381f94208fb935573632b1bdaf00a1ea29f87f27
lyukidon <qgx99098@gmail.com>
2021-05-16 23:41:05 +0900

    깊이 별 데이터를 이용해 그래프 구하기

--- question/quest1.js
+++ question/quest1.js
@@ -1,18 +1,21 @@
 //ㅠ는 3.14로 계산, g는 9.8로 계산
-function calculate(){
-    let height = +document.querySelector("input#height").value;
-    let period = +document.querySelector("input#period").value;
+function calculate(height, period){
     let kh = 0;
     while (((2*3.14/period)**2*height)/(9.8*kh) >= Math.tanh(kh)){
-        kh=kh+0.000001;
+        kh=kh+0.0001;
     }
     return kh;
 }
+function pushHT(){
+    let height = +document.querySelector("input#height").value;
+    let period = +document.querySelector("input#period").value;
+    return calculate(height, period);
+}
 document.querySelector('button#calculate').addEventListener('click',function(){
-    let answerkh = calculate();
-    let answerk = answerkh/+document.querySelector("input#height").value;
+    let answerkh = pushHT();
+    let answerk = +answerkh/+document.querySelector("input#height").value;
     let answerL = 2*3.14/answerk
     document.querySelector('span#answerkh').innerHTML = answerkh;
     document.querySelector('span#answerk').innerHTML = answerk;
     document.querySelector('span#answerL').innerHTML = answerL;
-})
+});
--- question/quest2.js
+++ question/quest2.js
@@ -1,35 +1,53 @@
-var ctx = document.querySelector('canvas#graph1').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, -5],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
+let xaxes = [];
+let yaxes = [];
+function makeArr(){
+    let period = +document.querySelector("input#period").value;
+    if(xaxes.length != 0){
+        xaxes = [];
+        yaxes = [];
+    }
+    for(let i=0; i <= 1000; i=i+100){
+        xaxes.push(i);
+        yaxes.push(calculate(i,period));
+    }
+    draw1();
+}
+//graph
+function draw1(){
+    var ctx = document.querySelector('canvas#graph1').getContext('2d');
+    var graph1 = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: xaxes,
+            datasets: [{
+                label: '파장',
+                data: yaxes,
+                borderColor: "blue",
+                fill:false,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                xAxes: [{
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: '깊이'
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    ticks: {
+                        suggestedMin: 0,
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: '파장 L'
+                    }
+                }]
             }
         }
-    }
-});
+    });
+}
+document.querySelector('button#makeGraph').addEventListener('click', makeArr);
